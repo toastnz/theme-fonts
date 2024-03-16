9e11c6c44e69953fac9252132eb7b67ffc08ff51
Emmanuel Davies <emmanuel@toast.co.nz>
2024-03-16 13:13:06 +1300

    onload event added to font links so the stylesheet loads to the page

--- src/Extensions/SiteConfigExtension.php
+++ src/Extensions/SiteConfigExtension.php
@@ -91,7 +91,7 @@ class SiteConfigExtension extends DataExtension
             // Make sure the value is not empty
             if (!$font || empty($font)) continue;
             // Add the preload link
-            $html .= '<link rel="preload" href="' . $font . '" onload="this.onload=null;this.rel=\'stylesheet\'">';
+            $html .= '<link rel="preload" href="' . $font . '" onload="this.onload=null;this.rel=\'stylesheet\'" as="style">';
         }
 
         // Preload the FontFiles
