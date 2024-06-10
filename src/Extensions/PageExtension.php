ff3a93f07d1215a6e60018092e620e3fa8c93cc7
Emmanuel Davies <emmanuel@toast.co.nz>
2024-06-10 16:36:03 +1200

    returning the preconnect fonts as part of the page getThemeFonts method

--- src/Extensions/PageExtension.php
+++ src/Extensions/PageExtension.php
@@ -25,14 +25,26 @@ class PageControllerExtension extends Extension
             $theme = ($siteID == 1) ? 'mainsite' : 'subsite-' . $siteID;
             $folderPath = Config::inst()->get(SiteConfig::class, 'css_folder_path');
             $fonts = $folderPath . $theme . '-theme-fonts.html';
+            $baseFolder = Director::baseFolder();
 
             if ($fonts){
-                if (!file_exists(Director::baseFolder() . $fonts)){
+                if (!file_exists($baseFolder . $fonts)){
                     $result = Helper::generateRequiredFiles($fonts);
                 }
 
-                if (file_exists(Director::baseFolder() . $fonts)) {
-                    return file_get_contents(Director::baseFolder() . $fonts);
+                if (file_exists($baseFolder . $fonts)) {
+                    $html = '';
+
+                    // Check if the siteconfig ThemeFontPreconnects field has been set
+                    if ($siteConfig->ThemeFontPreconnects) {
+                        $html .= $siteConfig->ThemeFontPreconnects;
+                    }
+
+                    // Add the fonts html
+                    $html .= file_get_contents($baseFolder . $fonts);
+
+                    // Return the html
+                    return $html;
                 }
             }
         }
