6e8e0b008b4781d212dbf2e6ff9759de563dd113
emmanuel-toast <emmanuel@toast.co.nz>
2024-11-28 08:41:43 +1300

    bug fix

--- src/Extensions/PageExtension.php
+++ src/Extensions/PageExtension.php
@@ -2,6 +2,7 @@
 
 namespace Toast\ThemeFonts\Extensions;
 
+use SilverStripe\ORM\DataObject;
 use SilverStripe\Core\Extension;
 use SilverStripe\Control\Director;
 use SilverStripe\ORM\DataExtension;
