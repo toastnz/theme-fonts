ed73ecb225383d4b28335f3644502cd965b4573c
emmanuel-toast <emmanuel@toast.co.nz>
2024-11-28 08:34:56 +1300

    bug fix

--- src/Extensions/DatabaseAdminExtension.php
+++ src/Extensions/DatabaseAdminExtension.php
@@ -5,7 +5,6 @@ namespace Toast\ThemeFonts\Extensions;
 use SilverStripe\Control\Director;
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\Security\Security;
-use Toast\ThemeFonts\Helpers\Helper;
 
 class DatabaseAdminExtension extends DataExtension
 {
