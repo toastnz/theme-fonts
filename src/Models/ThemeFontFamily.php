db68862faa9b94f429d10c90696bf3ccb4db1a43
emmanuel-toast <emmanuel@toast.co.nz>
2025-01-08 18:28:25 +1300

    reverting

--- src/Models/ThemeFontFamily.php
+++ src/Models/ThemeFontFamily.php
@@ -161,7 +161,7 @@ class ThemeFontFamily extends DataObject
 
         // Add the configuration field once the font files have been uploaded
         if ($this->ThemeFontFiles()->count()) {
-            $fields->addFieldToTab('Root.Main', $fontsField);
+            $fields->addFieldToTab('Root.Files', $fontsField);
         }
 
         return $fields;
